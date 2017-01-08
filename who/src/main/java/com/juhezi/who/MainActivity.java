9a25e7182d36ac43db464854a5aea29745b2c868
Juhezi <juhezix@163.com>
2017-01-08 16:43:40 +0800

    complete

--- who/src/main/java/com/juhezi/who/MainActivity.java
+++ who/src/main/java/com/juhezi/who/MainActivity.java
@@ -1,13 +1,19 @@
 package com.juhezi.who;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -32,10 +38,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Random random = new Random();
-                //产生0-100的随机数
-                int index = random.nextInt(100);
-                //向列表的数据中添加
-                whoAdapter.data.add(index + "");
+                int flag = 0;   //重复次数
+                while (true) {
+                    //产生0-100的随机数
+                    int index = random.nextInt(100);
+                    //判断是否已经存在
+                    if (whoAdapter.data.contains(index)) {  //已经存在
+                        flag++;
+                    } else {
+                        //向列表的数据中添加
+                        whoAdapter.data.add(index + "");
+                        break;
+                    }
+                    if (flag >= 16) {
+                        Toast.makeText(MainActivity.this,
+                                "休息会", Toast.LENGTH_SHORT)
+                                .show();
+                        break;
+                    }
+
+                }
                 //通知RecyclerView数据发生改变，让它显示新的数据
 //                whoAdapter.notifyDataSetChanged();
                 whoAdapter.notifyItemInserted(
@@ -44,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
                 // -1使因为位置是从0开始的
                 mRvList.scrollToPosition(
                         whoAdapter.data.size() - 1);
+                //为按钮添加旋转动画
+                ObjectAnimator rotateAnim =
+                        ObjectAnimator.ofFloat(mImgGet,
+                                "rotation", 0f, 180f);
+                ObjectAnimator scaleY =
+                        ObjectAnimator.ofFloat(mImgGet,
+                                "scaleY", 1f, 1.5f);
+                ObjectAnimator scaleX =
+                        ObjectAnimator.ofFloat(mImgGet,
+                                "scaleX", 1f, 1.5f);
+                AnimatorSet set = new AnimatorSet();
+                set.setDuration(500);
+                set.playTogether(rotateAnim, scaleX, scaleY);
+//                set.play(rotateAnim).after(scaleX).after(scaleY);
+                set.start();
             }
         });
         mBtnRestart.setOnClickListener(new View.OnClickListener() {
